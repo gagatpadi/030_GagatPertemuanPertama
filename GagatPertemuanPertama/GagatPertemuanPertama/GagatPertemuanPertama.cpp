8ee1e5039de49e3a54e7fc51ee8042c8bfca6fd8
gagatpadi <padiyogaradhana@gmail.com>
2024-02-27 18:49:48 +0700

    Add project files.

---
+++ GagatPertemuanPertama/GagatPertemuanPertama/GagatPertemuanPertama.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// numeric nJejari, nluas
+	float nJejari, nluas;
+	// display 'Masukan jejari= '
+	cout << "Masukan Jenis";
+	// accept nJejari
+	cin >> nJejari;
+	// compute nluas = 3,14 * r * r
+	nluas = 3.14 * nJejari * nJejari;
+	// display 'Luasnya= ' + nLuas
+	cout << "Luasnya" << nluas;
+}
